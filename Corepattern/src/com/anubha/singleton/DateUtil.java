d0043ddc26bb8afdb8fc5124bf68b41497bac827
Anubha <anubhasahay0206@gmail.com>
2023-01-17 13:33:45 +0530

    add the class

--- Corepattern/src/com/anubha/singleton/DateUtil.java
+++ Corepattern/src/com/anubha/singleton/DateUtil.java
@@ -1,5 +1,43 @@
 package com.anubha.singleton;
 
-public class DateUtil {
+public class DateUtil implements Cloneable {
+
+	private static DateUtil instance; // Lazy Initialization
+
+	// Eager Initialization
+	// private static DateUtil instance = new DateUtil();
+
+	// Eager Initialization using static block
+	// private static DateUtil instance;
+
+//	static {
+//		instance = new DateUtil();
+//	}
+
+	private DateUtil() {
+
+	}
+
+	public static DateUtil getInstance() {
+
+		if (instance == null) {
+
+			synchronized (DateUtil.class) {
+				// Lazy Initialization
+				if (instance == null) {
+					instance = new DateUtil();
+				}
+			}
+			;
+
+		}
+
+		return instance;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
 
 }
